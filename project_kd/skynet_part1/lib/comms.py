5dd141342c619502e75cce924e8b292fedf3bfee
kristy234 <kristy234@gmail.com>
2014-04-28 17:26:12 +1000

    Got rid of loop in recv func

--- project_kd/skynet_part1/lib/comms.py
+++ project_kd/skynet_part1/lib/comms.py
@@ -49,10 +49,10 @@ class StealthConn(object):
         if self.shared_hash != None:
             h = HMAC.new(self.shared_hash)
             h.update(data)
-            h.update(b'aaaa')
+            #h.update(b'aaaa')
             if self.verbose:
                 print("Hex digest is:",h.hexdigest())
-            mac_data = h.hexdigest() + data.decode("ascii")
+            mac_data = h.hexdigest()[:-1] + "a" + data.decode("ascii")
         else:
             mac_data = data
         if self.verbose:
@@ -83,38 +83,45 @@ class StealthConn(object):
 
 
     def recv(self):
-        while True:
-			# Decode the data's length from an unsigned two byte int ('H')
-            pkt_len_packed = self.conn.recv(struct.calcsize('H'))
-            unpacked_contents = struct.unpack('H', pkt_len_packed)
-            pkt_len = unpacked_contents[0]
-            if self.verbose:
-                print("Packet length is",pkt_len)
+        # Decode the data's length from an unsigned two byte int ('H')
+        pkt_len_packed = self.conn.recv(struct.calcsize('H'))
+        unpacked_contents = struct.unpack('H', pkt_len_packed)
+        pkt_len = unpacked_contents[0]
+        if self.verbose:
+            print("Packet length is",pkt_len)
 
-            encrypted_data = self.conn.recv(pkt_len)
+        encrypted_data = self.conn.recv(pkt_len)
+        if self.verbose:
+            print("Received Encrypted Data:",encrypted_data)
+        if self.cipher:
+            data = self.cipher.decrypt(encrypted_data)
             if self.verbose:
-                print("Received Encrypted Data:",encrypted_data)
-            if self.cipher:
-                data = self.cipher.decrypt(encrypted_data)
+                print("Receiving packet of length {}".format(pkt_len))
+                print("Encrypted data: {}".format(repr(encrypted_data)))
+                print("Original data: {}".format(data))
+        else:
+            data = encrypted_data
+        if self.verbose:
+            print("Decrypted Data:",data)
+
+        #strip off the HMAC and verify the message
+        if self.shared_hash != None:
+            h = HMAC.new(self.shared_hash)
+            hmac = data[:h.digest_size*2]
+            data = data[h.digest_size*2:]
+            h.update(data)
+            if h.hexdigest() != str(hmac, 'ascii'):
                 if self.verbose:
-                    print("Receiving packet of length {}".format(pkt_len))
-                    print("Encrypted data: {}".format(repr(encrypted_data)))
-                    print("Original data: {}".format(data))
-            else:
-                data = encrypted_data
-            if self.verbose:
-                print("Decrypted Data:",data)
-
-            #strip off the HMAC and verify the message
-            if self.shared_hash != None:
-                h = HMAC.new(self.shared_hash)
-                hmac = data[:h.digest_size*2]
-                data = data[h.digest_size*2:]
-                h.update(data)
-                if h.hexdigest() != str(hmac, 'ascii'):
+                    print("Bad message")
+                    print("HMAC from message:",str(hmac,'ascii'))
+                    print("HMAC from digest:",h.hexdigest())
+                    print("Not verifying message:",data)
                     print("Returning none...bad message?")
-                    continue;     
-            return data
+        elif self.verbose:
+            print("Shared hash is null")
+
+                 
+        return data
 
     def close(self):
         self.conn.close()
