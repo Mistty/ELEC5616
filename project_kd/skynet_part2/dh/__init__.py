a409c0ff15b18c0c2a2c2d1ab981d8745ac54335
Dean Pisani <dpisani93@gmail.com>
2014-05-12 09:32:02 +1000

    Created part 2 folder

---
+++ project_kd/skynet_part2/dh/__init__.py
@@ -0,0 +1,45 @@
+from Crypto.Hash import SHA256
+from Crypto.Random import random
+
+from lib.helpers import read_hex
+
+# 3073-bit safe prime from RFC 3526
+raw_prime = """FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+670C354E 4ABC9804 F1746C08 CA18217C 32905E46 2E36CE3B
+E39E772C 180E8603 9B2783A2 EC07A28F B5C55DF0 6F4C52C9
+DE2BCBF6 95581718 3995497C EA956AE5 15D22618 98FA0510
+15728E5A 8AAAC42D AD33170D 04507A33 A85521AB DF1CBA64
+ECFB8504 58DBEF0A 8AEA7157 5D060C7D B3970F85 A6E1E4C7
+ABF5AE8C DB0933D7 1E8C94E0 4A25619D CEE3D226 1AD2EE6B
+F12FFA06 D98A0864 D8760273 3EC86A64 521F2B18 177B200C
+BBE11757 7A615D6C 770988C0 BAD946E2 08E24FA0 74E5AB31
+43DB5BFC E0FD108E 4B82D120 A93AD2CA FFFFFFFF FFFFFFFF"""
+
+# Convert from the value supplied in the RFC to an integer
+prime = read_hex(raw_prime)
+g = 5	# Prime generator (not in original code)
+
+def create_dh_key():
+    # Creates a Diffie-Hellman key
+    # Returns (public, private)
+    private = random.randint(0,prime)
+    public  = pow(g, private, prime)
+    return (public, private)
+
+def calculate_dh_secret(their_public, my_private):
+    # Calculate the shared secret
+    shared_secret = pow(their_public,my_private,prime)
+
+    # Hash the value so that:
+    # (a) There's no bias in the bits of the output
+    #     (there may be bias if the shared secret is used raw)
+    # (b) We can convert to raw bytes easily
+    # (c) We could add additional information if we wanted
+    shared_hash = SHA256.new(bytes(str(shared_secret), "ascii")).hexdigest()
+    return shared_hash
---
+++ project_kd/skynet_part2/master_view.py
@@ -0,0 +1,18 @@
+import os
+
+
+def decrypt_valuables(f):
+    # TODO: For Part 2, you'll need to decrypt the contents of this file
+    # The existing scheme uploads in plaintext
+    # As such, we just convert it back to ASCII and print it out
+    decoded_text = str(f, 'ascii')
+    print(decoded_text)
+
+
+if __name__ == "__main__":
+    fn = input("Which file in pastebot.net does the botnet master want to view? ")
+    if not os.path.exists(os.path.join("pastebot.net", fn)):
+        print("The given file doesn't exist on pastebot.net")
+        os.exit(1)
+    f = open(os.path.join("pastebot.net", fn), "rb").read()
+    decrypt_valuables(f)
