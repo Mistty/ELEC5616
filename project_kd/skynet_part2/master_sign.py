b628c30aec9d2a8fd118e3d1117228e6bc309d16
Kristy Hughes <kristy234@gmail.com>
2014-05-12 08:42:10 +1000

    Opened the new project

---
+++ project_kd/skynet_part2/master_sign.py
@@ -0,0 +1,22 @@
+import os
+
+
+def sign_file(f):
+    # TODO: For Part 2, you'll use public key crypto here
+    # The existing scheme just ensures the updates start with the line 'Caesar'
+    # This is naive -- replace it with something better!
+    return bytes("Caesar\n", "ascii") + f
+
+
+if __name__ == "__main__":
+    fn = input("Which file in pastebot.net should be signed? ")
+    if not os.path.exists(os.path.join("pastebot.net", fn)):
+        print("The given file doesn't exist on pastebot.net")
+        os.exit(1)
+    f = open(os.path.join("pastebot.net", fn), "rb").read()
+    signed_f = sign_file(f)
+    signed_fn = os.path.join("pastebot.net", fn + ".signed")
+    out = open(signed_fn, "wb")
+    out.write(signed_f)
+    out.close()
+    print("Signed file written to", signed_fn)
