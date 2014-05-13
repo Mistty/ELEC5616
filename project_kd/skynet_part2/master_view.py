cc9f3cde33d86b1fa3a14c2827d3aeb8d366cc51
kristy234 <kristy234@gmail.com>
2014-05-13 10:34:57 +1000

    Changed master_view to check that it is not in plain text first

--- project_kd/skynet_part2/master_view.py
+++ project_kd/skynet_part2/master_view.py
@@ -5,23 +5,29 @@ from Crypto.Hash import SHA
 from Crypto import Random
 
 def decrypt_valuables(f):
-    # Get the private key and get associated hash digest size
-    key = RSA.importKey(open('TOP_SECRET_KEYS/master_rsa').read())
-    dsize = SHA.digest_size
-    sentinel = Random.new().read(15+dsize)
-    # Make a cipher object to decrypt with
-    cipher = PKCS1_v1_5.new(key)
-    # Decode the text
-    decoded_text = cipher.decrypt(f, sentinel)
-    # Grab the digest and if the digest is right, print the decrypted data
-    digest = SHA.new(decoded_text[:-dsize]).digest()
-    if digest==decoded_text[-dsize:]:
-        # Remove the digest from the message
-        decoded_text=decoded_text[:-dsize]
-        decoded_text = str(decoded_text, 'ascii')
-        print(decoded_text)
-    else:
-        print("Bad encryption")
+    # Check that the text is actually encoded first...
+    try:
+        print(str(f,'ascii'))
+        print("This text was not encrypted!")
+    # Now decrypt it with RSA
+    except UnicodeDecodeError:
+        # Get the private key and get associated hash digest size
+        key = RSA.importKey(open('TOP_SECRET_KEYS/master_rsa').read())
+        dsize = SHA.digest_size
+        sentinel = Random.new().read(15+dsize)
+        # Make a cipher object to decrypt with
+        cipher = PKCS1_v1_5.new(key)
+        # Decode the text
+        decoded_text = cipher.decrypt(f, sentinel)
+        # Grab the digest and if the digest is right, print the decrypted data
+        digest = SHA.new(decoded_text[:-dsize]).digest()
+        if digest==decoded_text[-dsize:]:
+                # Remove the digest from the message
+                decoded_text=decoded_text[:-dsize]
+                decoded_text = str(decoded_text, 'ascii')
+                print(decoded_text)
+        else:
+                print("Bad encryption")
 
 
 if __name__ == "__main__": 
